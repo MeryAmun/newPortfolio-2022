6f2a153f8c869b4d41068edd57fbd762360d6db8
MeryAmun <chebesiawah@gmail.com>
2022-03-14 01:52:13 +0100

    header and styles done

---
+++ src/Assets/index.js
@@ -0,0 +1,9 @@
+import cv from './images/cv.pdf';
+import me from './images/wendy.png';
+
+
+
+
+export {
+    cv, me
+}
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import { me } from '../../Assets';
+import CTA from './CTA';
 import './header.css'
+import HeaderSocials from './HeaderSocials';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+     <div className="container header__container">
+   <h5>Hello I'm</h5>
+   <h1>Chebesi EdyLionel Awah</h1>
+   <h5 className="text-light">
+     Full Stack Developer
+   </h5>
+   <CTA/>
+   <HeaderSocials/>
+
+   <div className="me">
+     <img src={me} alt="Me" />
+   </div>
+
+   <a href="#contact" className="scroll__down">Scroll Down</a>
+     </div>
+    </header>
   )
 }
 
