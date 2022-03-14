78f56240e9e65d5fb7d95a06c5096cdb4b79981e
MeryAmun <chebesiawah@gmail.com>
2022-03-14 02:40:19 +0100

    navbar

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './nav.css'
+import {AiOutlineHome, AiOutlineUser,BiBook,RiServiceLine,BiMessageSquareDetail} from './index'
+
 
 const Nav = () => {
+  const [activeNow, setActiveNow] = useState('#')
   return (
-    <div>Nav</div>
+   <nav>
+     <a href="#" onClick={() => setActiveNow('#')} className={activeNow === '#' ? 'active' : ''}><AiOutlineHome/></a>
+     <a href="#about" onClick={() => setActiveNow('#about')} className={activeNow === '#about' ? 'active' : ''}><AiOutlineUser/></a>
+     <a href="#experience" onClick={() => setActiveNow('#experience')}  className={activeNow === '#experience' ? 'active' : ''}><BiBook/></a>
+     <a href="#services" onClick={() => setActiveNow('#services')}  className={activeNow === '#services' ? 'active' : ''}><RiServiceLine/></a>
+     <a href="#contact" onClick={() => setActiveNow('#contact')}  className={activeNow === '#contact' ? 'active' : ''}><BiMessageSquareDetail/></a>
+   </nav>
   )
 }
 
