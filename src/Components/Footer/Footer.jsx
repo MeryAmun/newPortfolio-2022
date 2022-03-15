31e3682030ce018217c52c78b273aaa6a85b8a1e
MeryAmun <chebesiawah@gmail.com>
2022-03-15 01:36:06 +0100

    footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
 import './footer.css'
+import { FaFacebookF, FiGithub,IoLogoLinkedin } from '../Nav';
 
 const Footer = () => {
   return (
-    <section id="footer">Footer</section>
+    <footer>
+      <a href="#" className="footer__logo">CHEBS</a>
+<ul className="permalinks">
+  <li><a href="#"></a>Home</li>
+  <li><a href="#about"></a>About</li>
+  <li><a href="#experience"></a>Experience</li>
+  <li><a href="#portfolio"></a>Portfolio</li>
+  <li><a href="#contact"></a>Contact</li>
+</ul>
+<div className="footer__socials">
+<li><a href="https://web.facebook.com/suso.che"><FaFacebookF/></a></li>
+<li><a href="https://github.com/MeryAmun?tab=overview&from=2022-03-01&to=2022-03-14"><FiGithub/></a></li>
+<li><a href="https://www.linkedin.com/in/chebesi-awah-b891331ab/"></a><IoLogoLinkedin/></li>
+</div>
+<div className="footer__copyright">
+  <small>&copy; CHEBS Portfolio 2022. All Rights Reserved</small>
+</div>
+    </footer>
   )
 }
 
--- src/Components/Nav/index.js
+++ src/Components/Nav/index.js
@@ -10,10 +10,14 @@ import { BsPatchCheckFill } from 'react-icons/bs';
 import { MdOutlineEmail } from 'react-icons/md';
 import { RiMessengerLine } from 'react-icons/ri';
 import { BsWhatsapp } from 'react-icons/bs';
+import { FaFacebookF } from "react-icons/fa";
+import { FiGithub } from "react-icons/fi";
+import { IoLogoLinkedin } from "react-icons/io";
 
 
 
 export {
     AiOutlineHome,AiOutlineUser,BiBook, RiServiceLine,BiMessageSquareDetail,
-    FaAward, FiUsers,VscFolderLibrary,BsPatchCheckFill, MdOutlineEmail, RiMessengerLine, BsWhatsapp
+    FaAward, FiUsers,VscFolderLibrary,BsPatchCheckFill,
+     MdOutlineEmail, RiMessengerLine, BsWhatsapp,FaFacebookF,FiGithub,IoLogoLinkedin
 }
