03518de2153fece003dd8c411239f4c2cbd78529
MeryAmun <chebesiawah@gmail.com>
2022-03-15 02:30:23 +0100

    test

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -5,18 +5,18 @@ import { FaFacebookF, FiGithub,IoLogoLinkedin } from '../Nav';
 const Footer = () => {
   return (
     <footer>
-      <a href="#" className="footer__logo">CHEBS</a>
+      <a href="#" className="footer__logo">____________________</a>
 <ul className="permalinks">
-  <li><a href="#"></a>Home</li>
-  <li><a href="#about"></a>About</li>
-  <li><a href="#experience"></a>Experience</li>
-  <li><a href="#portfolio"></a>Portfolio</li>
-  <li><a href="#contact"></a>Contact</li>
+  <li><a href="#">Home</a></li>
+  <li><a href="#about">About</a></li>
+  <li><a href="#experience">Experience</a></li>
+  <li><a href="#portfolio">Portfolio</a></li>
+  <li><a href="#contact">Contact</a></li>
 </ul>
 <div className="footer__socials">
-<li><a href="https://web.facebook.com/suso.che"><FaFacebookF/></a></li>
-<li><a href="https://github.com/MeryAmun?tab=overview&from=2022-03-01&to=2022-03-14"><FiGithub/></a></li>
-<li><a href="https://www.linkedin.com/in/chebesi-awah-b891331ab/"></a><IoLogoLinkedin/></li>
+  <a href="https://web.facebook.com/suso.che" target='_blank'><FaFacebookF/></a>
+<a href="https://github.com/MeryAmun?tab=overview&from=2022-03-01&to=2022-03-14" target='_blank'><FiGithub/></a>
+<a href="https://www.linkedin.com/in/chebesi-awah-b891331ab/" target='_blank'><IoLogoLinkedin/></a>
 </div>
 <div className="footer__copyright">
   <small>&copy; CHEBS Portfolio 2022. All Rights Reserved</small>
