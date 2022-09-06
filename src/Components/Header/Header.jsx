7e71618a81a7b31f06e8181b87a71ca0d7eb6e30
MeryAmun <chebesiawah@gmail.com>
2022-09-06 19:56:58 +0100

    updated resume

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -8,17 +8,16 @@ const Header = () => {
   return (
     <header>
      <div className="container header__container">
+       
    <h5>Hello I'm</h5>
    <h1>Chebesi EdyLionel Awah</h1>
    <h5 className="text-light">
      Full Stack Developer
    </h5>
-   <CTA/>
+ 
    <HeaderSocials/>
 
-   <div className="me">
-     {/* <img src={me} alt="Me" className='header__image'/> */}
-   </div>
+   <CTA/>
 
    <a href="#contact" className="scroll__down">Scroll Down</a>
      </div>
